3c7a74568dcfa34200ccafc467cfac890103a716
Thomas Kohler <g2t@e.mail.de>
2022-03-25 17:16:15 +0100

    fix, stabilize, introduce skipping when value unchanged

--- drivers/i2c/pcf8591_driver.go
+++ drivers/i2c/pcf8591_driver.go
@@ -2,7 +2,9 @@ package i2c
 
 import (
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"gobot.io/x/gobot"
 )
@@ -11,12 +13,7 @@ import (
 // The default address applies when all address pins connected to ground.
 const pcf8591DefaultAddress = 0x48
 
-const (
-	pcf8591_CHANMASK = 0x03
-	pcf8591_AIMASK   = 0x04 // only relevant for ADC
-	pcf8591_ADMASK   = 0x30
-	pcf8591_DAMASK   = 0x40
-)
+const pcf8591Debug = true
 
 type mode uint8
 type channel uint8
@@ -28,7 +25,7 @@ const (
 	pcf8591_CHAN3         = 0x03
 )
 
-const pcf8591_AION = 0x04 // auto increment
+const pcf8591_AION = 0x04 // auto increment, only relevant for ADC
 
 const (
 	pcf8591_ALLSINGLE mode = 0x00
@@ -37,6 +34,8 @@ const (
 	pcf8591_TWODIFF        = 0x30
 	pcf8591_ANAON          = 0x40
 )
+
+const pcf8591_ADMASK = 0x33 // channels and mode
 
 type modeChan struct {
 	mode    mode
@@ -81,6 +80,9 @@ var modeMap = map[string]modeChan{
 	"t.2-3": {pcf8591_THREEDIFF, pcf8591_CHAN2},
 }
 
+const rotateCount = 0 // usefully for debugging purposes
+const skipReads = 3
+
 // PCF8591Driver is a Gobot Driver for the PCF8591 8-bit 4xA/D & 1xD/A converter with i2c interface and 3 address pins.
 // The analog inputs can be used as differential inputs in different ways.
 //
@@ -94,12 +96,18 @@ var modeMap = map[string]modeChan{
 // For example, here is the Adafruit board that uses this chip:
 // https://www.adafruit.com/product/4648
 //
+// This driver was tested with Tinkerboard and this board with temperature & brightness sensor:
+// https://www.makershop.de/download/YL_40_PCF8591.pdf
+//
 type PCF8591Driver struct {
 	name       string
 	connector  Connector
 	connection Connection
 	Config
 	gobot.Commander
+	lastCtrlByte uint8
+	lastAnaOut   uint8
+	LastRead     [rotateCount*4 + 1][]byte
 }
 
 // NewPCF8591Driver creates a new driver with specified i2c interface
@@ -152,48 +160,100 @@ func (p *PCF8591Driver) Halt() (err error) {
 }
 
 // AnalogRead returns value from analog reading of given input description
+//
 // Vlsb = (Vref-Vagnd)/256
 // values are related to Vlsb by (Van+ - Van-)/Vlsb, Van-=0 for single mode
+//
+// The first read contains the last converted value (usually the last read).
+// After the channel was switched this means the value of the previous read channel.
 // After power on, the first byte read will be 80h, because the read is one cycle behind.
+//
+// Important note:
+// With a bus speed of 100 kBit/sec, the ADC conversion has ~80 us + ACK (time to transfer the previous value).
+// This time seems to be a little bit to small (datasheet 90 us).
+// A active bus driver don't fix it (it seems rather the opposite).
+//
+// This leads to following behavior:
+// * the transition process takes an additional cycle, very often
+// * some circuits takes one cycle longer transition time in addition
+// * reading more than one byte by Read([]byte), e.g. to calculate an average, is not sufficient,
+//   because some missing integration steps in each byte (each byte is a little bit to small)
+//
+// So, for default, we drop the first three bytes to get the right value.
 func (p *PCF8591Driver) AnalogRead(description string) (value int, err error) {
-	mc, ok := modeMap[description]
-	if !ok {
-		descriptions := []string{}
-		for k := range modeMap {
-			descriptions = append(descriptions, k)
-		}
-		ds := strings.Join(descriptions, ", ")
-		return 0, fmt.Errorf("Unknown description '%s' for read analog value, accepted values: %s", description, ds)
-	}
-
-	// ANAON is needed for ADC
-	ctrlByte := uint8(pcf8591_ANAON | uint8(mc.mode) | uint8(mc.channel))
-	if err = p.connection.WriteByte(ctrlByte); err != nil {
-		return 0, err
-	}
-	unsignedVal, err := p.connection.ReadByte()
+	mc, err := pcf8591GetModeChannel(description)
 	if err != nil {
 		return 0, err
 	}
 
-	value = int(unsignedVal)
-	if mc.isDiff() {
+	// reset channel and mode
+	ctrlByte := p.lastCtrlByte & ^uint8(pcf8591_ADMASK)
+	// set to current channel and mode, AI must be off, because we need reading twice
+	ctrlByte = ctrlByte | uint8(mc.mode) | uint8(mc.channel) & ^uint8(pcf8591_AION)
+	if err = p.writeCtrlByte(ctrlByte); err != nil {
+		return 0, err
+	}
+
+	// initiate read but skip some bytes
+	for i := uint8(0); i < rotateCount*4+1; i++ {
+		p.readBuf(i)
+	}
+
+	// additional relax time
+	time.Sleep(1 * time.Millisecond)
+
+	// real read
+	uval, err := p.connection.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+
+	value = int(uval)
+	if mc.pcf8591IsDiff() {
 		value = value - 128
 	}
 
 	return value, err
 }
 
+func (p *PCF8591Driver) readBuf(nr uint8) error {
+	buf := make([]byte, skipReads)
+	cntRead, err := p.connection.Read(buf)
+	if err != nil {
+		return err
+	}
+	if cntRead != len(buf) {
+		return fmt.Errorf("Not enough bytes (%d of %d) read", cntRead, len(buf))
+	}
+	if pcf8591Debug {
+		p.LastRead[nr] = buf
+	}
+	return nil
+}
+
 // AnalogWrite writes the given value to the analog output (DAC)
 // Vlsb = (Vref-Vagnd)/256
 // Vaout = Vagnd+Vlsb*value
 func (p *PCF8591Driver) AnalogWrite(value uint8) (err error) {
-	if err = p.AnalogOutputState(true); err != nil {
+	if p.lastAnaOut == value {
+		if pcf8591Debug {
+			log.Printf("write skipped because value unchanged: 0x%X\n", value)
+		}
+		return nil
+	}
+
+	ctrlByte := p.lastCtrlByte | pcf8591_ANAON
+	cntWritten, err := p.connection.Write([]byte{ctrlByte, value})
+	if err != nil {
 		return err
 	}
-	if err = p.connection.WriteByte(value); err != nil {
-		return err
+
+	if cntWritten != 2 {
+		return fmt.Errorf("Not enough bytes (%d of %d) written", cntWritten, 2)
 	}
+
+	p.lastCtrlByte = ctrlByte
+	p.lastAnaOut = value
 	return nil
 }
 
@@ -204,17 +264,46 @@ func (p *PCF8591Driver) AnalogWrite(value uint8) (err error) {
 func (p *PCF8591Driver) AnalogOutputState(state bool) (err error) {
 	var ctrlByte uint8
 	if state {
-		ctrlByte = uint8(pcf8591_DAMASK | pcf8591_ANAON)
+		ctrlByte = p.lastCtrlByte | pcf8591_ANAON
 	} else {
-		ctrlByte = uint8(pcf8591_DAMASK & ^pcf8591_ANAON)
+		ctrlByte = p.lastCtrlByte & ^uint8(pcf8591_ANAON)
 	}
-	if err = p.connection.WriteByte(ctrlByte); err != nil {
+
+	if err = p.writeCtrlByte(ctrlByte); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (mc modeChan) isDiff() bool {
+func (p *PCF8591Driver) writeCtrlByte(ctrlByte uint8) error {
+	if p.lastCtrlByte != ctrlByte {
+		if err := p.connection.WriteByte(ctrlByte); err != nil {
+			return err
+		}
+		p.lastCtrlByte = ctrlByte
+	} else {
+		if pcf8591Debug {
+			log.Printf("write skipped because control byte unchanged: 0x%X\n", ctrlByte)
+		}
+	}
+	return nil
+}
+
+func pcf8591GetModeChannel(description string) (*modeChan, error) {
+	mc, ok := modeMap[description]
+	if !ok {
+		descriptions := []string{}
+		for k := range modeMap {
+			descriptions = append(descriptions, k)
+		}
+		ds := strings.Join(descriptions, ", ")
+		return nil, fmt.Errorf("Unknown description '%s' for read analog value, accepted values: %s", description, ds)
+	}
+
+	return &mc, nil
+}
+
+func (mc modeChan) pcf8591IsDiff() bool {
 	switch mc.mode {
 	case pcf8591_TWODIFF:
 		return true
--- drivers/i2c/pcf8591_driver_test.go
+++ drivers/i2c/pcf8591_driver_test.go
@@ -9,6 +9,7 @@ import (
 func initTestPCF8591DriverWithStubbedAdaptor() (*PCF8591Driver, *i2cTestAdaptor) {
 	adaptor := newI2cTestAdaptor()
 	pcf := NewPCF8591Driver(adaptor)
+	pcf.lastCtrlByte = 0xFF // prevent skipping of write
 	pcf.Start()
 	return pcf, adaptor
 }
@@ -16,26 +17,27 @@ func initTestPCF8591DriverWithStubbedAdaptor() (*PCF8591Driver, *i2cTestAdaptor)
 func TestPCF8591DriverAnalogReadSingle(t *testing.T) {
 	// sequence to read the input channel:
 	// * prepare value (with channel and config) and write control register
+	// * read 3 values to drop (see description in implementation)
 	// * read the analog value
-	//
-	// TODO: there seems to be no possibility to read the control register
-	// so possibly we have to store it by our own
-	// TODO: it is possible to read more than one value without repeat the
-	// start and control byte process until next write or read from another channel.
-	// So, possibly we can speedup reading by compare the last written controlbyte?
 	//
 	// arrange
 	pcf, adaptor := initTestPCF8591DriverWithStubbedAdaptor()
 	adaptor.written = []byte{} // reset writes of Start() and former test
 	description := "s.1"
-	wantCtrlByteVal := uint8(pcf8591_ANAON | pcf8591_ALLSINGLE | pcf8591_CHAN1)
-	returnRead := uint8(0xFF)
-	want := int(returnRead)
+	pcf.lastCtrlByte = 0x00
+	ctrlByteOn := uint8(pcf8591_ALLSINGLE | pcf8591_CHAN1)
+	returnRead := []uint8{0x01, 0x02, 0x03, 0xFF}
+	want := int(returnRead[3])
 	// arrange reads
 	numCallsRead := 0
 	adaptor.i2cReadImpl = func(b []byte) (int, error) {
 		numCallsRead++
-		b[0] = returnRead
+		if numCallsRead == 1 {
+			b = returnRead[0 : len(b)-1]
+		}
+		if numCallsRead == 2 {
+			b[0] = returnRead[len(returnRead)-1]
+		}
 		return len(b), nil
 	}
 	// act
@@ -43,8 +45,8 @@ func TestPCF8591DriverAnalogReadSingle(t *testing.T) {
 	// assert
 	gobottest.Assert(t, err, nil)
 	gobottest.Assert(t, len(adaptor.written), 1)
-	gobottest.Assert(t, adaptor.written[0], wantCtrlByteVal)
-	gobottest.Assert(t, numCallsRead, 1)
+	gobottest.Assert(t, adaptor.written[0], ctrlByteOn)
+	gobottest.Assert(t, numCallsRead, 2)
 	gobottest.Assert(t, got, want)
 }
 
@@ -54,24 +56,24 @@ func TestPCF8591DriverAnalogReadDiff(t *testing.T) {
 	// * read the analog value
 	// * convert to 8-bit two's complement (-127...128)
 	//
-	// TODO: there seems to be no possibility to read the control register
-	// so possibly we have to store it by our own
-	// TODO: it is possible to read more than one value without repeat the
-	// start and control byte process until next write or read from another channel.
-	// So, possibly we can speedup reading by compare the last written controlbyte?
-	//
 	// arrange
 	pcf, adaptor := initTestPCF8591DriverWithStubbedAdaptor()
 	adaptor.written = []byte{} // reset writes of Start() and former test
 	description := "m.2-3"
-	wantCtrlByteVal := uint8(pcf8591_ANAON | pcf8591_MIXED | pcf8591_CHAN2)
-	returnRead := uint8(0xFF)
-	want := int(returnRead) - 128
+	pcf.lastCtrlByte = 0x00
+	ctrlByteOn := uint8(pcf8591_MIXED | pcf8591_CHAN2)
+	returnRead := []uint8{0x01, 0x02, 0x03, 0xFF}
+	want := int(returnRead[3]) - 128
 	// arrange reads
 	numCallsRead := 0
 	adaptor.i2cReadImpl = func(b []byte) (int, error) {
 		numCallsRead++
-		b[0] = uint8(returnRead)
+		if numCallsRead == 1 {
+			b = returnRead[0 : len(b)-1]
+		}
+		if numCallsRead == 2 {
+			b[0] = returnRead[len(returnRead)-1]
+		}
 		return len(b), nil
 	}
 	// act
@@ -79,54 +81,51 @@ func TestPCF8591DriverAnalogReadDiff(t *testing.T) {
 	// assert
 	gobottest.Assert(t, err, nil)
 	gobottest.Assert(t, len(adaptor.written), 1)
-	gobottest.Assert(t, adaptor.written[0], wantCtrlByteVal)
-	gobottest.Assert(t, numCallsRead, 1)
+	gobottest.Assert(t, adaptor.written[0], ctrlByteOn)
+	gobottest.Assert(t, numCallsRead, 2)
 	gobottest.Assert(t, got, want)
 }
 
 func TestPCF8591DriverAnalogWrite(t *testing.T) {
 	// sequence to write the output:
-	// * write ANAON to the control register => done by AnalogOutputState(true)
-	// * write the analog value
-	//
-	// TODO: there seems to be no possibility to read the control register
-	// so possibly we have to store it by our own
-	// TODO: it is possible to write more than one analog value without repeat the
-	// start and control byte process until next read. So, possibly we can speedup
-	// writing with an boolean marker (e.g. writePrepared) or simply compare the controlbyte?
+	// * create new value for the control register (ANAON)
+	// * write the control register and value
 	//
 	// arrange
 	pcf, adaptor := initTestPCF8591DriverWithStubbedAdaptor()
 	adaptor.written = []byte{} // reset writes of Start() and former test
-	wantCtrlByteVal := uint8(pcf8591_DAMASK | pcf8591_ANAON)
+	pcf.lastCtrlByte = 0x00
+	pcf.lastAnaOut = 0x00
+	ctrlByteOn := uint8(pcf8591_ANAON)
 	want := uint8(0x15)
+	// arrange writes
+	adaptor.i2cWriteImpl = func(b []byte) (int, error) {
+		return len(b), nil
+	}
 	// act
 	err := pcf.AnalogWrite(want)
 	// assert
 	gobottest.Assert(t, err, nil)
 	gobottest.Assert(t, len(adaptor.written), 2)
-	gobottest.Assert(t, adaptor.written[0], wantCtrlByteVal)
+	gobottest.Assert(t, adaptor.written[0], ctrlByteOn)
 	gobottest.Assert(t, adaptor.written[1], want)
 }
 
 func TestPCF8591DriverAnalogOutputState(t *testing.T) {
 	// sequence to set the state:
-	// * create the new value (ctrlByte) of the control register (ANAOFF, ANAON)
+	// * create the new value (ctrlByte) for the control register (ANAON)
 	// * write the register value
-	//
-	// TODO: there seems to be no possibility to read the control register
-	// so possibly we have to store it by our own
 	//
 	// arrange
 	pcf, adaptor := initTestPCF8591DriverWithStubbedAdaptor()
-	ctrlByteOn := uint8(pcf8591_DAMASK | pcf8591_ANAON)
-	ctrlByteOff := uint8(pcf8591_DAMASK & ^pcf8591_ANAON)
 	for bitState := 0; bitState <= 1; bitState++ {
 		adaptor.written = []byte{} // reset writes of Start() and former test
 		// arrange some values
-		wantCtrlByteVal := ctrlByteOn
+		pcf.lastCtrlByte = uint8(0x00)
+		wantCtrlByteVal := uint8(pcf8591_ANAON)
 		if bitState == 0 {
-			wantCtrlByteVal = ctrlByteOff
+			pcf.lastCtrlByte = uint8(0xFF)
+			wantCtrlByteVal = uint8(0xFF & ^pcf8591_ANAON)
 		}
 		// act
 		err := pcf.AnalogOutputState(bitState == 1)
